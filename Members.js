07ffe6cc31f5f9748f3aba82dcf3dda70e498e02
Illés Tóth <illestothvisualz@gmail.com>
2021-01-28 11:52:37 +0100

    creating middleware/routes/public folder

---
+++ Members.js
@@ -0,0 +1,24 @@
+const members = [
+    {
+        id: 1,
+        name: "Tóth József",
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: "Kovács Mária",
+        status: 'active'
+    },
+    {
+        id: 3,
+        name: "Szerb Antal",
+        status: 'passive'
+    },
+    {
+        id: 4,
+        name: "Varga Emese",
+        status: 'active'
+    }
+];
+
+module.export = members;
